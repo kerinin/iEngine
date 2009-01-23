fa96ea65fc73886fe0662f915f792ceb9d73f217
Ryan Michael <kerinin@gmail.com>
2009-01-22 23:11:26 -0600

    Very close now.  Points are classified into two distinct clusters, but SV's are ending up in odd 1-off clusters.
    Probably an error in the adjacency search algorithm

--- components.py
+++ components.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-import sys, getopt, math, datetime, os
+import sys, getopt, math, datetime, os, cmath
 from math import sqrt, log
 
 from numpy import *
@@ -73,28 +73,28 @@ class inference_module:
 		N = zeros([d,d])
 		for i in range(d):
 			for j in range(i,d):
-				val = self.norm(self.SV[i].data, self.SV[j].data)
-				N[i,j] = val
-				N[j,i] = val
+				if i==j:
+					N[i,j] = -1
+				else:
+					val = self.norm(self.SV[i].data, self.SV[j].data)
+					N[i,j] = val
+					N[j,i] = val
 				
 		# Calculate R^2
 		# R^2(x) = K_(x,x) - 2 sum_j{\beta_j K(x_j,x)} + sum_{i,j}{\beta_i \beta_j K(x_i,x_j)
 		betas = array( [ [ sv.beta for sv in self.SV] ] )
+		R2 = complex( 1 - 2* ( dot( N[:1:], betas.T ) ) + ( dot( dot(betas, N), betas.T) )[0,0] )
 		
-		R2 = self.K(N[0,0]) - 2* ( dot( betas.T, N[:1:] ) ) + ( dot( dot(betas, N), betas.T) )
-		
-		#NOTE: the problem here is that the arrays are 1-d...
-				
 		# Calculate Z
 		#Z = \sqrt{ -\frac{ln( \sqrt{ 1-R^2} )}{q} }
-		Z = sqrt( -1* log( sqrt( 1- R2 ** 2 ) )  / self.gamma )
+		Z =  abs( cmath.sqrt( -1* cmath.log( cmath.sqrt( 1- ( R2 ** 2 ) ) )  / self.gamma ) )
 		
 		# Determine a 'good' gamma starting point
 		self.gamma_start = 1/N.max()
 		M = N < Z
 		
 		# Assign SV's to clusters
-		self.cluster_count = 0
+		self.cluster_count = -1
 		def r(base,offset,stack):
 			for j in range(offset,d):	
 				if not self.SV[j].cluster and M[offset,j]:
@@ -109,10 +109,10 @@ class inference_module:
 			self.SV[i].SV_array = 1/N[i]*self.inhibition
 
 			if not self.SV[i].cluster:
+				self.cluster_count += 1
 				self.SV[i].cluster = self.cluster_count
 				stack = list()
 				r(i,i,stack)
-				self.cluster_count += 1
 				
 class data_vector:
 	def __init__(self,data,*args,**kargs):
--- iEngine.py
+++ iEngine.py
@@ -32,16 +32,19 @@ def run():
 		data.append( array( [gauss(2.0,.1), gauss(0.0,.1) ]) )
 	for i in range(60):
 		data.append( array( [gauss(0.0,.1), gauss(2.0,.1) ]) )
-
-	param = svm_parameter(svm_type=ONE_CLASS, kernel_type = RBF,gamma=1e-2,=1e10)
+		
+	print "Optimizing Coeffiecients"
+	# large gamma = small cluster
+	# large nu = lots of SV
+	param = svm_parameter(svm_type=ONE_CLASS, kernel_type = RBF,gamma=1,nu=.1)
 	prob = svm_problem( range(120), data)
 	m= svm_model(prob,param)
 	m.save('output.svm')
 	
+	print "Parsing Output"
 	mod = inference_module('output.svm')
 	
-	clusters = [ None , ]*mod.cluster_count
-	
+	clusters = [ None , ]*(mod.cluster_count+1)
 	colors = ['b','r','g']
 	for point in data:
 		vector = mod.classify( data_vector(point) )
@@ -51,18 +54,25 @@ def run():
 				clusters[vector.cluster] = [vector,]
 			else:
 				clusters[vector.cluster].append(vector)
+				
+	print "Formatting Results"
 	for cluster in clusters:
 		if cluster:
-			x = list()
-			y = list()
-			for point in cluster:
-				x.append( point.data[0] )
-				y.append( point.data[1] )
-			scatter(x,y,c=colors[cluster[0].cluster % 3], label="Cluster %s" % cluster[0].cluster)
+			scatter( [point.data[0] for point in cluster], [point.data[1] for point in cluster],s=10,c=colors[cluster[0].cluster % 3])
+		
+	clustersSV = [ None , ]*(mod.cluster_count+1)	
+	for SV in mod.SV:
+		if not clustersSV[SV .cluster]:
+			clustersSV[SV .cluster] = [SV ,]
+		else:
+			clustersSV[SV .cluster].append(SV )
+	for cluster in clustersSV:
+		scatter( [SV.data[0] for SV in cluster],  [SV.data[1] for SV in cluster],  s=30,  c=colors[cluster[0].cluster % 3],  label="Cluster %s" % cluster[0].cluster )
 			
-	title('%s clusters from %s points' % (mod.cluster_count,len(data)))
+	title('%s SV in %s clusters from %s points' % (len(mod.SV),mod.cluster_count+1,len(data)))
 	legend()
 	show()
+	
 
 def help():
 	print __doc__
