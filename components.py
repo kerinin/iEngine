b2d59a3a15865db7ece954f496111012318e1d67
Ryan Michael <kerinin@gmail.com>
2009-01-24 15:02:52 -0600

    refactored and seems to be working

--- components.py
+++ components.py
@@ -5,36 +5,84 @@ from math import sqrt, log
 
 import numpy
 from numpy import *
-import scipy
+from scipy import spatial
 from networkx import *
 from pylab import *
-frim svm import*
+from svm import *
 	
+class data_vector:
+	def __init__(self,data,*args,**kargs):
+		self.SV_matrix = None
+		self.cluster = None
+		self.data = array(data)
+	
+class support_vector(data_vector):
+	def __init__(self,beta,*args,**kargs):
+		self.beta = beta
+		data_vector.__init__(self,*args,**kargs)
+		
 class inference_module:
-	def __init__(self,param=svm_parameter(svm_type=ONE_CLASS, kernel_type = RBF),data=list()):
-		self.param = param
+	def __init__(self,data=list(),gamma=None,nu=1e-10):
+		self.param = svm_parameter(svm_type=ONE_CLASS, kernel_type = RBF)
 		self.svm = None
 		self.data = data
 		self.SV = list()
 		self.kernel = None
 		self.rho = None
 		self.clusters = list()
-		
-		if data:
-			self.dump(data)
-			self.compute()
+
+		if nu:
+			self.param.nu = nu
+		if gamma:
+			self.param.gamma = gamma
+		if self.data:
+			self._compute()
 	
-	def compute(self,path='output.svm'):
-		# calculate gamma (if not defined)
-		self.gamma_start = 1/N.max()
+	def __iadd__(self, points):
+		# overloaded '+=', used for adding a vector list to the module's data
+		# 
+		# @param points		A LIST of observation vectors (not a single ovservation)
+		self.data += points
+	
+	def induce(self,vector,append=True):
+		# Induce from vector
+		# Creates an abstract representation of the given point using inductive inference based on similarity to previously observed points
+		# 
+		# @param point		The observation vector for which to create an abstract representation
+		# @param append		Adds the point to the module's data set if True
 		
-		self.svm = svm_model(svm_problem( range(len(self.data)), data ),param) )
+		if append:
+			self.data.append(vector)
+			
+		self._check_recompute()
+		
+		#point.SV_array = self._anorm( vector.data, array([SV.data for SV in self.SV]) )
+		vector.SV_array = array( spatial.distance.cdist( [vector.data,], [SV.data for SV in self.SV] ) )
+		
+		if self.svm.predict(vector.data) > 0:
+			vector.cluster = self.SV[ vector.SV_array.argmin() ].cluster
+		
+		return vector
+		
+	def deduce(self,abstraction):
+		# Deduce a Probability Distribution Vector 
+		# Creates a Probability Distribution Vector from the given abstract representation using deductive inference based on previously observed vectors.
+		pass
+		
+		
+	def _check_recompute(self):
+		if self.data and not self.SV:
+			self._compute()
+		
+	def _compute(self,path='output.svm'):
+		if not self.param.gamma:
+			self.param.gamma = 1/spatial.distance.pdist(self.data, 'euclidean').max()
+		
+		self.svm = svm_model(svm_problem( range(len(self.data)), self.data ),self.param)
 		self.svm.save(path)
 		
 		parse_file = file(path,'r')
 		lines = parse_file.readlines()
-		self.kernel = lines[1].split(' ')[1]
-		self.gamma = float( lines[2].split(' ')[1] )
 		self.rho = float( lines[5].split(' ')[1] )
 
 		for line in lines[7:]:
@@ -51,41 +99,34 @@ class inference_module:
 			support_vector(beta,data)
 			self.SV.append( support_vector(beta,data) )
 			
-		self.boundaries()
+		self._boundaries()
+		print "gamma=%s" % self.param.gamma
+		print "nu=%s" % self.param.nu
 			
-	def get_SV(self):
-		for sv in self.SV:
-			print sv
-			
-	def anorm(self,x,y):
+	def _anorm(self,x,y):
 		return numpy.sqrt(((x - y)**2).T.sum(0))
 		
-	def norm(self,x,y):
+	def _norm(self,x,y):
 		return sqrt(((x - y)**2).sum())
 		
-	def K(self,norm):
+	def _K(self,norm):
 		return exp(-(norm**2)/self.gamma)
 		
-	def classify(self,point):
-		point.SV_array = self.anorm( point.data, array([SV.data for SV in self.SV]) )
-		point.cluster = self.SV[ point.SV_array.argmin() ].cluster
-		
-		return point
-		
-	def boundaries(self):
+	def _boundaries(self):
 	# determine cluster boundaries and add any clusters which do not yet exist
 	
 		# Construct SV adjacency matrix
 		d = len(self.SV)
-		N = zeros([d,d])
-		for i in range(d):
-			for j in range(i,d):
-				if i==j:
-					N[i,j] = -1
-				else:
-					val = self.norm(self.SV[i].data, self.SV[j].data)
-					N[i,j] = val
-					N[j,i] = val
+		N = spatial.distance.squareform( spatial.distance.pdist( [SV.data for SV in self.SV] , 'euclidean' ) )
+		#N = zeros([d,d])
+		#for i in range(d):
+		#	for j in range(i,d):
+		#		if i==j:
+		#			N[i,j] = -1
+		#		else:
+		#			val = self._norm(self.SV[i].data, self.SV[j].data)
+		#			N[i,j] = val
+		#			N[j,i] = val
 				
 		# Calculate R^2
 		# R^2(x) = K_(x,x) - 2 sum_j{\beta_j K(x_j,x)} + sum_{i,j}{\beta_i \beta_j K(x_i,x_j)
@@ -97,7 +138,7 @@ class inference_module:
 		
 		# Calculate Z
 		#Z = \sqrt{ -\frac{ln( \sqrt{ 1-R^2} )}{q} }
-		Z =  abs( cmath.sqrt( -1* cmath.log( cmath.sqrt( 1- ( R2 ** 2 ) ) )  / self.gamma ) )
+		Z =  abs( cmath.sqrt( -1* cmath.log( cmath.sqrt( 1- ( R2 ** 2 ) ) )  / self.param.gamma ) )
 
 		# Assign SV's to clusters
 		G = Graph( N <= Z )
@@ -109,14 +150,5 @@ class inference_module:
 				self.SV[j].cluster = i
 				self.clusters[i].append(self.SV[j])
 				
-class data_vector:
-	def __init__(self,data,*args,**kargs):
-		self.SV_matrix = None
-		self.cluster = None
-		self.data = array(data)
-	
-class support_vector(data_vector):
-	def __init__(self,beta,*args,**kargs):
-		self.beta = beta
-		data_vector.__init__(self,*args,**kargs)
+
 			
--- iEngine.py
+++ iEngine.py
@@ -1,23 +1,14 @@
 #! /usr/bin/env python
 
 import sys, getopt, math, datetime, os
-from math import sqrt, sin
 from random import gauss
 
-from svm import *
+from santa_fe import getData
+
+from components import *
 
 from numpy import *
 from pylab import *
-
-from cvxopt.base import *
-from cvxopt.blas import dot 
-from cvxopt.solvers import qp
-
-from cvxopt import solvers
-solvers.options['show_progress'] = False
-
-from santa_fe import getData
-from components import *
 
 _Functions = ['run']
 	
@@ -26,41 +17,41 @@ def run():
 	
 	print "Loading Dataset"
 	# Retrieve dataset
-	data = getData('B1.dat')[:100]
-	#data = list()
-	#for i in range(100):
-	#	data.append( array( [gauss(2.0,.1), gauss(0.0,.1) ]) )
-	#for i in range(100):
-	#	data.append( array( [gauss(0.0,.1), gauss(2.0,.1) ]) )
+	#data = getData('B1.dat')[:100]
+	data = list()
+	for i in range(100):
+		data.append( array( [gauss(2.0,.1), gauss(0.0,.1) ]) )
+	for i in range(100):
+		data.append( array( [gauss(0.0,.1), gauss(2.0,.1) ]) )
 		
-	print "Optimizing Coeffiecients"
-	# large gamma = small cluster
-	# large nu = lots of SV
+	mod = inference_module(data,gamma=10, nu=.1)
 	
-	print "Parsing Output"
-	mod = inference_module('output.svm')
-	
-	clusters = [ None , ]*(len(mod.clusters))
+	clusters = [ [] , ]*(len(mod.clusters))
+	bsv = list()
 	colors = ['b','r','g','c','m','y','w']
 
 	for point in data:
-		vector = mod.classify( data_vector(point) )
-
-		if not clusters[vector.cluster]:
-			clusters[vector.cluster] = [vector,]
+		vector = mod.induce( data_vector(point),False)
+		
+		if vector.cluster != None:
+			if not clusters[vector.cluster]:
+				clusters[vector.cluster] = [vector,]
+			else:
+				clusters[vector.cluster].append(vector)
 		else:
-			clusters[vector.cluster].append(vector)
+			bsv.append(vector)
 				
 	print "Formatting Results"
-	print "Suggested gamma: %s" % mod.gamma_start
 	for cluster in clusters:
-		scatter( [point.data[0] for point in cluster], [point.data[1] for point in cluster],s=10,  c=colors[cluster[0].cluster % 7])
+		if cluster:
+			scatter( [point.data[0] for point in cluster], [point.data[1] for point in cluster],s=10,  c=colors[cluster[0].cluster % 7])
 		
 	for cluster in mod.clusters:
 		scatter( [SV.data[0] for SV in cluster],  [SV.data[1] for SV in cluster],  s=30,  c=colors[cluster[0].cluster % 7],  label="Cluster %s" % cluster[0].cluster )
 			
+	scatter( [v.data[0] for v in bsv], [v.data[1] for v in bsv], s=10, marker="x" )
+			
 	title('%s SV in %s clusters from %s points' % (len(mod.SV),len(mod.clusters),len(data)))
-	legend()
 	show()
 	
 
