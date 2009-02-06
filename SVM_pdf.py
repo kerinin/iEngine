9a3b2efe86a71fd2b2939635251ec303a5ea94c6
Ryan Michael <kerinin@gmail.com>
2009-02-05 18:53:02 -0600

    It appears to be working - time to test the actual output.  Only problem is everything is a SV.  maybe default parameter values

--- SVM_pdf.py
+++ SVM_pdf.py
@@ -13,7 +13,7 @@ from numpy import *
 _Functions = ['run']
 	
 class svm:
-	def __init__(self,data=list(),C = 1, epsilon = .5, rho = 1e-4, L = .5):
+	def __init__(self,data=list(),C = .01, epsilon = .5, rho = 1e-4, L = .5):
 		self.data = data
 		self.W = None
 		
@@ -46,7 +46,7 @@ class svm:
 		t = ( ( self.data.T > self.data ).sum(1,dtype=float) / len(self.data) ).reshape( len(self.data), 1)	# Since F is an estimate of the target value t, i'm simply renaming it
 		
 		# Set learning rate \rho and randomly set w_i
-		W = numpy.random.rand( len(self.data), 1)
+		W = numpy.random.rand( len(self.data), 1) * .9 + .1
 		
 		# Calculate covariance matrix K and let \sigma_i^2 = K_{ii}
 		# \Lambda = variable (gamma)
@@ -58,16 +58,15 @@ class svm:
 		def inner():
 			# Inner Loop
 			# yX = \langle y(x) \rangle = \sum_{i=1}^N w_i K(x, x_i )
-			yX = dot(K[0].reshape( 1,len(self.data) ),W)[0][0]
+			yX = K[0].reshape( [ len(self.data), 1] ) * W
 			
 			# yXi = \langle y(s) \rangle_i = yX - \sigma_i^2 w_i
 			yXi = yX - sigma2 * W
-			
+
 			# Fi = C/2 exp( C/2 ( 2yXi - 2t_i + 2\epsilon + C \sigma_i^2 ) )
 			#	* ( 1 - erf( ( yXi - t_i + \epsilon + C \sigma_i^2 ) / sqrt( 2 \sigma_i^2 ) )
 			#	- C/2 exp( C/2 ( -2yXi + 2 t_i + 2 \epsilon + C \sigma_i^2 ) )
 			#	* ( 1 - erf( -C/2 ( 2yXi - 2t_i + 2\epsilon + C \sigma_i^2 ) )
-
 			Fi = (
 				self.C/2. * exp( (self.C/2.) * ( ( 2. * yXi ) - ( 2. * t ) + ( 2. * self.epsilon ) + ( self.C * sigma2 ) ) ) 
 				* ( 1. - scipy.special.erf( ( yXi - t + self.epsilon + ( self.C * sigma2 ) ) / sqrt( 2. * sigma2 ) ) )
@@ -92,47 +91,45 @@ class svm:
 
 			# erf(x) = 2/sqrt(\pi) \sum_0^x e^{-t^2} dt (see scipy.special.erf)
 			# w_i = w_i + \rho ( ( F_i / G_i ) - w_i )
-			return ( t, yXi, Gi, self.rho * ( Fi / Gi - W ) )
+			F_safe = Fi * ( Gi > 0 )
+			G_safe = Gi + ( Gi == 0 )
+			return ( t, yXi, Gi, self.rho * ( F_safe / G_safe - W ) )
 				
 		start = datetime.datetime.now()
 		
 		# Outer Loop
 		while True:
+			
 			for i in range(10):
 				W += inner()[3]
+				W *= W > 0
+			
 			(t,yXi,Gi,W_delta) = inner()
 			
 			# IG_i = 1/2 erf( ( t_i - \leftangle y(x_i) \rightangle_i + \epsilon ) / sqrt( 2 \sigma_i^2 ) ) - 1/2 erf( ( t_i - \leftangle y(x_i) \rightangle_i - \epsilon ) )
 			IG = .5 * scipy.special.erf( ( t - yXi + self.epsilon ) / sqrt( 2 * sigma2 ) ) - .5 * scipy.special.erf( ( t - yXi - self.epsilon ) / sqrt( 2 * sigma2 ) )
-			print IG.shape
 			
 			# Z = C^2 - w_i^2 - \frac{ w_i \leftangle y(x_i) \rightangle_i  + \sigma_i^2 C^2 + IG_i }{ \sigma_i^2 G( \leftangle y(x_i) \rightangle_i, \sigma_i^2 ) }
 			Z = ( self.C * 2 ) - ( W ** 2 ) - ( ( ( W * yXi ) + ( sigma2 * ( self.C ** 2 ) ) + IG ) / ( sigma2 * Gi ) )
-			print Z.shape
 			
 			# \Sigma_i = - \sigma_i^2 - ( Z )^{-1}
 			Sigma_i = -sigma2 - ( 1 / Z )
-			print Sigma_i.shape
 			
 			# \Sigma = diag( \Sigma_1,...,\Sigma_N )
-			Sigma = diag( Sigma_i.reshape(len(self.data) ) )
-			print Sigma.shape
+			Sigma = diag( Sigma_i.reshape([len(self.data),] ) )
 			
 			# sigma_i^2 = \frac{ 1 } { [ ( \Sigma + K ) ^{-1} ]_{ii} } - \Sigma_i
 			sigma2 = ( 1 / ( 1 / ( Sigma + K ) ).diagonal().reshape([len(self.data),1]) ) - Sigma_i
-			print sigma2.shape
 			
-			break
-			
-			if absolute(W_delta).sum() < 1e-6:
+			if absolute(W_delta).sum() < 1e-4:
 				self.W = W + W_delta
 				break
 			else:
 				print "Cumulative adjustment of Coefficients: %s" % absolute(W_delta).sum()
 				W += W_delta
 				
-		print "*** Optimization completed in %ss" % (datetime.datetime.now() - start).seconds
-		print W
+		#print "*** Optimization completed in %ss" % (datetime.datetime.now() - start).seconds
+		print W.T
 		self.W = W
 		
 def run():
