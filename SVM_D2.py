fba45fadee21f0a0afc5423b991f6768a36c0b34
Ryan Michael <kerinin@gmail.com>
2009-02-23 17:58:04 -0600

    executin without error, but producing a lot of loss.  probably has errors.  Need new graphs

--- SVM_D2.py
+++ SVM_D2.py
@@ -84,8 +84,11 @@ class svm:
 	# @param Y				[Mxd] array of observations
 	# @param gamma			kernel width parameter
 	
-		N = X.size
-		M = Y.size
+		N,d1 = X.shape
+		M,d2 = Y.shape
+		
+		if d1 != self.d != d2:
+			raise StandardError, 'Matrices do not conform to the dimensionality of existing observations'
 		
 		# Sigmoid
 		return ( 1.0 / ( 1.0 + numpy.exp( -gamma * ( X.reshape([N,1,self.d])-Y.reshape([1,M,self.d]) ) ) ) ).prod(2).reshape(N,M)
@@ -100,8 +103,11 @@ class svm:
 	# @param Y				[Mxd] array of observations
 	# @param gamma			kernel width parameter
 	
-		N = X.shape[0]
-		M = Y.shape[0]
+		N,d1 = X.shape
+		M,d2 = Y.shape
+		
+		if d1 != self.d != d2:
+			raise StandardError, 'Matrices do not conform to the dimensionality of existing observations'
 		
 		diff = X.reshape([N,1,self.d])-Y.reshape([1,M,self.d])
 		
@@ -138,20 +144,12 @@ class svm:
 	
 	def _compute(self):
 		start = datetime.datetime.now()
-
-		# NOTE: From (1), the optimization problem should be:
-		# min ( \sum_{i=1}^\ell ( y_i - \sum_{j=1}^\ell \sum_{n=1}^k \alpha_j^n k_n(x_i,x_j) )^2 + \lambda \sum_{i=1}^\ell \sum_{n=1}^k \frac{1}{\gamma_n} \alpha_i^n )
-		# sjt \alpha_i \ge 0, i = 1,...,\ell
-		
-		# Gameplan: implement this minimization problem - if it works figure out what the matrix
-		# definitions will be for the optimization problem and re-implement it in CVXOPT.  From
-		# there you can start working on decomposition.
 		
 		kappa = len( self.gamma )
 		(N,self.d) = self.X.shape
 		self.Y = ( ( .5 + (self.X.reshape(N,1,self.d) > transpose(self.X.reshape(N,1,self.d),[1,0,2])).prod(2).sum(1,dtype=float) ) / N ).reshape([N,1])
-		self.K = numpy.hstack(  [self._K( self.X, self.X.T, gamma ) for gamma in self.gamma] )
-		self.Gamma = numpy.hstack( [ numpy.tile(g,N) for g in self.gamma ] )
+		self.K = numpy.hstack(  [self._K( self.X, self.X, gamma ) for gamma in self.gamma] )
+		self.Gamma = numpy.repeat(self.gamma,N).reshape([N*kappa,1])
 		
 		P = cvxopt.matrix( numpy.dot(self.K.T,self.K), (N*kappa,N*kappa) )
 		q = cvxopt.matrix( ( self._Omega(self.Gamma) - ( numpy.ma.dot( self.K.T, self.Y ) ) ), (N*kappa,1) )
@@ -168,93 +166,30 @@ class svm:
 		self.NSV = beta.count()
 		self.alpha = beta
 		self.beta = beta.compressed().reshape([self.NSV,1])
-		self.SV = numpy.ma.array( numpy.tile(self.X.T,kappa).T, mask=mask).compressed().reshape([self.NSV,d])
+		self.SV = numpy.ma.array( numpy.tile(self.X.T,kappa).T, mask=numpy.repeat(mask,self.d)).compressed().reshape([self.NSV,self.d])
 		self.Gamma = numpy.ma.array( self.Gamma, mask=mask ).compressed().reshape([self.NSV,1])
 		
 		duration = datetime.datetime.now() - start
 		print "optimized in %ss" % ( duration.seconds + float(duration.microseconds)/1000000)
 		
 def run():
-	samples = array([[gauss(0,1)] for i in range(50) ] + [[gauss(8,1)] for i in range(50) ]).reshape([50,2]) 
-	#samples = array([[gauss(0,1)] for i in range(80) ] ).reshape([80,1]) 
-	#samples = arange(0,4).reshape([4,1])
-	#samples = array( [ [i,i+.1,i+.2] for i in range(0,10) ], dtype=float ).reshape([30,1])
-	#samples = list()
-
-	start = -5.
-	end = 12.
-	X = arange(start,end,.25)
 	fig = plt.figure()
 	
-	#C = [0.,1e-5,1e-4,1e-3,1e-2,1e-1,1e0,1e1]
-	#C = [1e-11,1e-10,1e-9,1e-8,1e-7,1e-6,1e-5,1e-4,1e-3,1e-2,1e-1,1e0]
-	#C = [1e-7,1e-6,1e-5,1e-4,1e-3,1e-2,1e-1,1e0,1e1,1e2,1e3,1e4,1e5,1e6,1e7]
-	C = arange(1e-6,1e-0,1e-1) 
-	
-	res = [ svm( samples, Lambda=c, gamma=[.125,.5,2.,8.,32.]) for c in C ]
-	
-	#plt.plot( log10(C), [ mod.NSV for mod in res ], 'o--' )
-	a = fig.add_subplot(1,2,1)
-	a.plot(numpy.sort( res[0].X,0), numpy.sort( res[0].Y,0), 'green' )
-	for mod in res:
-		a.plot(X, mod.cdf(X), '--' )
-	a.set_title("Computed vs emprical CDF")
-	
-	b = fig.add_subplot(1,2,2)
-	b.plot( [mod.NSV for mod in res], [(mod.cdf_res()**2).sum() for mod in res], 'o--')
-	b.set_title("NSV vs Loss")
-	
-	plt.show()
-	return True
-	
-	
-	# 2 good, 10 bad
-	# 5->4
-	# 4->4
-	# 3->3
-	
+	samples = array([[gauss(0,1)] for i in range(50) ] + [[gauss(8,1)] for i in range(50) ]).reshape([50,2]) 
 	mod = svm( samples,Lambda=1e-8, gamma=[.125,.25,.5,1.,2.,4.,8.,16.,32.] )
-	
 	print mod
 	
 	a = fig.add_subplot(2,2,1)
-	#a.hist(mod.K.compressed().flatten(), 20, normed=1)
-	#a.set_title("K distribution")
 	#a.plot( [ i % mod.N for i in range( mod.N * len(mod.gamma) ) ], mod.alpha, 'o' )
 	#a.set_title("weights (x=ell)")
 	
-	a.plot(mod.Gamma, mod.beta,  'o')
-	a.set_title('gamma vs weight')
+	#b = fig.add_subplot(2,2,3)
 	
-	#a.hist(mod.cdf_res(), 20, normed=1)
-	#a.axvline(x=0)
-	#a.set_title('residual distribution')
+	#c = fig.add_subplot(2,2,2)
 	
+	#d = fig.add_subplot(2,2,4)
 	
-	b = fig.add_subplot(2,2,3)
-	#n, bins, patches = b.hist(mod.X, 20, normed=1, facecolor='green', alpha=0.5, label='empirical distribution')
-	#b.plot(X,mod.pdf(X), 'r--', label="computed distribution")
-	#b.set_title("Computed vs empirical PDF")
-	b.hist(mod.SV[0], 10 )
-	b.set_title('SV distribution')
-	
-	c = fig.add_subplot(2,2,2)
-	c.plot(numpy.sort(mod.X,0), numpy.sort(mod.Y,0), 'green' )
-	c.plot(X, mod.cdf(X), 'r--' )
-	c.plot( mod.X, mod.cdf_res(mod.X)**2, '+' )
-	c.grid(True)
-	#c.plot( mod.X, (mod.Y.reshape( [len(mod.X),]) - mod.cdf( mod.X.reshape( [len(mod.X),]) ) ) ** 2, '+' )
-	c.set_title("Computed vs emprical CDF")
-	
-	d = fig.add_subplot(2,2,4)
-	for i in range( mod.NSV ):
-		d.plot( X, numpy.dot( mod._K( atleast_2d(X).T, mod.SV[0][i], mod.Gamma[0][i] ), mod.beta[0][i].T ) )
-		#d.plot( X, numpy.dot( mod._K( X, mod.SV[i], mod.gamma[i] ), mod.beta[i] ) )
-	d.plot( mod.SV, mod.beta/2, 'o' )
-	d.grid(True)
-	d.set_title("SV Contributions")
-	
-	plt.show()
+	#plt.show()
 	
 	
 def help():
