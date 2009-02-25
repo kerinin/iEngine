73c6011bdbc999e229ac454ae0037bb0c50533b8
Ryan Michael <kerinin@gmail.com>
2009-02-25 13:09:51 -0600

    new file for decomposition
    --HG--
    rename : SVM_D2.py => SVM_D3.py

---
+++ SVM_D3.py
@@ -0,0 +1,275 @@
+#! /usr/bin/env python
+
+# Based on three papers:
+# (1) Support Vector Density Estimation, and
+# (2) Density Estimation using Support Vector Machines
+# (3) An Improved Training Algorithm for Support Vector Machines
+# (1) and (2) by Weston et. al, the first from '99 and the second from '98
+# They seem to be reprints of the same paper
+# (3) By Osuna et. al.
+
+# Specifically, this is intended to implement the decomposition algorithm 
+# described in (3) using the Support Vector Machine described in (1)-1.9
+
+# NOTE: The general approach here is going to be to implement the SVM
+# first, then work out the math for decomposing it.
+
+import sys, getopt, math, datetime, os, cmath
+from random import gauss
+
+import numpy
+import scipy
+import scipy.special
+import scipy.stats
+import cvxopt
+#import cvxmod
+from cvxopt import *
+
+from numpy import *
+
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import matplotlib.cm as cm
+
+
+
+cvxopt.solvers.options['show_progress'] = False
+cvxopt.solvers.options['abstol'] = 1e-15
+cvxopt.solvers.options['reltol'] = 1e-15
+cvxopt.solvers.options['feastol'] = 1e-15
+
+_Functions = ['run']
+	
+class svm:
+	def __init__(self,data=list(),Lambda=.1, gamma =arange(1,4,1) ):
+	# SVM Class
+	#
+	# @param data		[Nxd] array of observations where N is the number of observations and d is the dimensionality of the abstract space
+	# @param Lambda		Regularizer to control Smoothness / Accuracy.  Preliminary experimental results show the range 0-1 controls this parameter.
+	# @param gamma		List of gamma values which define the kernel smoothness
+	
+		try:
+			self.N,self.d = data.shape
+		except ValueError:
+			self.N,self.d = (len(self.X),1)
+			self.X = data.reshape([ self.N, self.d ])
+		else:
+			self.X = data
+		
+		self.Lambda = Lambda
+		self.gamma = gamma
+		
+		self.Gamma = None		# gamma repeated N times
+		self.Y = None				# empirical CDF of X
+		self.SV = None			# X value array of SV
+		self.NSV = None			# cardinality of SV
+		self.alpha = None			# the full weight array for all observations
+		self.beta = None			# weight array for SV
+		self.K = None				# precomputed kernel matrix
+		
+		self._compute()
+		
+	def __str__(self):
+		ret = "SVM Instance\n"
+		ret += "X: [%s x %sd]\n" % (self.N, self.d)
+		ret += "Lambda: %s\n" % self.Lambda
+		ret += "gamma: %s\n" % str(self.gamma)
+		ret += "SV: %s (%s percent)\n" % ( self.NSV,100. * float(self.NSV) / float(self.N ) )
+		ret += "Loss: %s\n" % (self.cdf_res()**2).sum()
+		return ret
+	
+	def _Omega(self,Gamma):
+	# Regularizer function
+	#
+	# @param Gamma			[N*kappa x 1] array of gamma values
+	
+		return self.Lambda * (Gamma)
+		
+	def _K(self,X,Y,gamma):
+	# Kernel function
+	#
+	# @param X				[Nxd] array of observations
+	# @param Y				[Mxd] array of observations
+	# @param gamma			kernel width parameter
+	
+		N,d1 = X.shape
+		M,d2 = Y.shape
+		
+		if d1 != self.d != d2:
+			raise StandardError, 'Matrices do not conform to the dimensionality of existing observations'
+		
+		diff = X.reshape([N,1,self.d]) - numpy.transpose( Y.reshape([M,1,self.d]), [1,0,2] )
+		
+		# Sigmoid
+		return ( 1.0 / ( 1.0 + numpy.exp( -gamma * diff ) ) ).prod(2).reshape(N,M)
+		
+		# RBF
+		#return ( exp( -((X-Y)**2.0) / gamma ) ).reshape(N,M)
+
+	def _k(self,X,Y,gamma):
+	# Cross-kernel function
+	#
+	# @param X				[Nxd] array of observations
+	# @param Y				[Mxd] array of observations
+	# @param gamma			kernel width parameter
+	
+		N,d1 = X.shape
+		M,d2 = Y.shape
+		
+		if d1 != self.d != d2:
+			raise StandardError, 'Matrices do not conform to the dimensionality of existing observations'
+		
+		diff = X.reshape([N,1,self.d])- numpy.transpose( Y.reshape([M,1,self.d]), [1,0,2] )
+		
+		return ( gamma / ( 2.0 + numpy.exp( gamma * diff ) + numpy.exp( -gamma * diff ) ) ).prod(2).reshape(N,M)
+		
+	def cdf(self,X):
+	# Cumulative distribution function
+	#
+	# @param X				[Nxd] array of points for which to calculate the CDF
+		
+		return numpy.dot( self._K( X, self.SV, self.Gamma ), self.beta )
+		
+	def pdf(self,X):
+	# Probability distribution function
+	#
+	# @param X				[Nxd] array of points for which to calculate the PDF
+	
+		return numpy.dot( self._k( X, self.SV, self.Gamma ), self.beta )
+		
+	def cdf_res(self,X=None):
+	# CDF residuals
+	#
+		if X==None:
+			X = self.X
+		#return ( self.Y.flatten() - self.cdf( X.flatten() ).flatten() )
+		return self.Y - self.cdf(X)
+		
+	def cdf_loss(self,X=None):
+		
+		return sqrt( (self.cdf_res(X)**2).sum(1) )
+		
+	def __iadd__(self, points):
+		# overloaded '+=', used for adding a vector list to the module's data
+		# 
+		# @param points		A LIST of observation vectors (not a single ovservation)
+		raise StandardError, 'Not Implemented'
+		self.X += points
+	
+	def _compute(self):
+		start = datetime.datetime.now()
+		
+		kappa = len( self.gamma )
+		(N,self.d) = self.X.shape
+		self.Y = ( ( .5 + (self.X.reshape(N,1,self.d) > transpose(self.X.reshape(N,1,self.d),[1,0,2])).prod(2).sum(1,dtype=float) ) / N ).reshape([N,1])
+		self.K = numpy.hstack(  [self._K( self.X, self.X, gamma ) for gamma in self.gamma] )
+		self.Gamma = numpy.repeat(self.gamma,N).reshape([N*kappa,1])
+		
+		P = cvxopt.matrix( numpy.dot(self.K.T,self.K), (N*kappa,N*kappa) )
+		q = cvxopt.matrix( ( self._Omega(self.Gamma) - ( numpy.ma.dot( self.K.T, self.Y ) ) ), (N*kappa,1) )
+		G = cvxopt.matrix( -identity(N*kappa), (N*kappa,N*kappa) )
+		h = cvxopt.matrix( 0.0, (N*kappa,1) )
+		A = cvxopt.matrix( 1., (1,N*kappa) )
+		b = cvxopt.matrix( 1., (1,1) )
+		
+		# Solve!
+		p = solvers.qp( P=P, q=q, G=G, h=h, A=A, b=b )
+		
+		beta = ma.masked_less( p['x'], 1e-8 )
+		mask = ma.getmask(beta)
+		self.NSV = beta.count()
+		self.alpha = beta
+		self.beta = beta.compressed().reshape([self.NSV,1])
+		self.SV = numpy.ma.array( numpy.tile(self.X.T,kappa).T, mask=numpy.repeat(mask,self.d)).compressed().reshape([self.NSV,self.d])
+		self.Gamma = numpy.ma.array( self.Gamma, mask=mask ).compressed().reshape([self.NSV,1])
+
+		duration = datetime.datetime.now() - start
+		print "optimized in %ss" % ( duration.seconds + float(duration.microseconds)/1000000)
+		
+def run():
+	fig = plt.figure()
+	
+	samples = vstack( [ numpy.random.multivariate_normal( mean=array([3,3]), cov=array( identity(2) ), size=array([50,]) ),
+		numpy.random.multivariate_normal( mean=array([7,7]), cov=array( identity(2) ), size=array([50,]) ) 
+	] )
+	
+	'''
+	C = arange(1e-6,1e-0,1e-1) 
+	res = [ svm( samples, Lambda=c, gamma=[.125,.5,2.,8.,32.]) for c in C ]
+	
+	a = fig.add_subplot(1,2,1)
+	a.plot( C, [mod.cdf_loss().sum() for mod in res],'r' )
+	a.set_title("Loss vs Lambda")
+	a.grid(True)
+	
+	b = fig.add_subplot(1,2,2)
+	b.plot( C, [mod.NSV for mod in res] )
+	b.set_title("NSV vs Lambda")
+	b.grid(True)
+	
+	c = fig.add_subplot(2,2,3)
+	c.plot( [mod.NSV for mod in res], [mod.cdf_loss().sum() for mod in res], 'o--' )
+	c.set_title("NSV vs Loss")
+	
+	
+	plt.show()
+	return True
+	'''
+	
+	mod = svm( samples, Lambda=.005, gamma=[.125,.25,.5,1,2,4,8,16] )
+	
+	print mod
+	
+	X = dstack(mgrid[0:10:.1,0:10:.1]).reshape([10000,2])
+	
+	plt.contourf(arange(0,10,.1),arange(0,10,.1),mod.pdf(X).reshape([100,100]).T,200, antialiased=True, cmap=cm.gray )
+	CS = plt.contour(arange(0,10,.1),arange(0,10,.1),mod.pdf(X).reshape([100,100]).T, [.01,], colors='r' )
+	plt.plot( hsplit(samples,2)[0],hsplit(samples,2)[1], 'r+' )
+	plt.scatter( hsplit(mod.SV,2)[0].reshape([mod.NSV,]),hsplit(mod.SV,2)[1].reshape([mod.NSV],), s=(mod.NSV*200*mod.beta.reshape([mod.NSV,])), alpha=.25, color='r' )
+	plt.clabel(CS, inline=1, fontsize=10)
+	
+	plt.axis( [0,10,0,10] )
+	plt.show()
+	
+	
+def help():
+	print __doc__
+	return 0
+	
+def process(arg='run'):
+	if arg in _Functions:
+		globals()[arg]()
+	
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+def main(argv=None):
+	if argv is None:
+		argv = sys.argv
+	try:
+		try:
+			opts, args = getopt.getopt(sys.argv[1:], "hl:d:", ["help","list=","database="])
+		except getopt.error, msg:
+			raise Usage(msg)
+		
+		# process options
+		for o, a in opts:
+			if o in ("-h", "--help"):
+				for f in _Functions:
+					if f in args:
+						apply(f,(opts,args))
+						return 0
+				help()
+		
+		# process arguments
+		for arg in args:
+			process(arg) # process() is defined elsewhere
+			
+	except Usage, err:
+		print >>sys.stderr, err.msg
+		print >>sys.stderr, "for help use --help"
+		return 2
+
+if __name__ == "__main__":
+	sys.exit(main())
