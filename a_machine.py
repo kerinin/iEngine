working towards KL divergence - getting a display but incorrect values

a_machine.py
@@ -15,6 +15,9 @@ gamma = 100
 
 def k(X,Y):
   return T.prod(T.exp( -gamma * T.pow(X-Y,2) ), 3)
+  
+def parzen(observations, test_points):
+  return T.sum( k(observations, test_points), 1) / observations.shape[1]
 
 # matrix types for parzen estimation between sets of points and sets of sequences
 col = T.TensorType('float32', [False, False,True,False])
@@ -22,8 +25,7 @@ row = T.TensorType('float32', [True, True,False,False])
 
 observations = col('parzen observations')
 test_points = row('parzen test points')
-#probabilities = T.prod( T.sum( k(observations, test_points), 1), 2) / observations.shape[1]
-probabilities = T.sum( k(observations, test_points), 1) / observations.shape[1]
+#probabilities = T.sum( k(observations, test_points), 1) / observations.shape[1]
 
 # observations should be in format [sequence][observation][1][dimension]
 # test_points should be in format [1][1][test_point][dimension]
@@ -35,14 +37,33 @@ probabilities = T.sum( k(observations, test_points), 1) / observations.shape[1]
 # 5 test points of 2 dimensions
 # test_points = np.random.rand(1,1,4,2)
 # parzen_function(observations, test_points) => result[a][b] = the probability of test point (b) given sequence (a)
-parzen_function = function( [observations, test_points], probabilities )
+parzen_function = function( [observations, test_points], parzen(observations, test_points) )
+	
 	
 
-# activation[vector index]
 # KL_divergence(P,Q) = sum_i P(i) log ( P(i) / Q(i) )
 # In this case, we'll use the points in 'other' as i
 #activation = - T.sum( parzen_function(sequences, other) * T.log( parzen_function(sequences,other) / parzen_function(other,other) ), 1 )
 #activation_function = function( [sequences,other], activation )
+
+col = T.TensorType('float32', [False, False])
+row = T.TensorType('float32', [True, False])
+
+p_values = col('P(i)')
+q_values = row('Q(i)')
+
+#divergence = - T.sum( parzen_function(P, test_points) * T.log( parzen_function(P, test_points) / parzen_function( Q, test_points ) ), 1 )
+#divergence = - T.sum( parzen(P, test_points) * T.log( parzen(P, test_points) / parzen( Q, test_points ) ), 1 )
+
+divergence = -T.sum( p_values * T.log( p_values / q_values ), 1 )
+kl_divergence_function = function( [p_values, q_values], divergence)
+
+def kl_divergence(P,Q,test_points):
+  p_values = parzen_function(P, test_points).astype('float32')
+  q_values = parzen_function(Q, test_points).astype('float32')
+  
+  return kl_divergence_function(p_values, q_values)
+  
 
 class model:
   def __init__(self):

run_a_machine.py
@@ -9,12 +9,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-_Functions = ['run']
+_Functions = ['run', 'test_parzen']
 	
 import theano.tensor as T
 from theano import function
 
 def run():
+  print "Starting"
+  
+  xrange = [0,1]
+  xstep = .01
+  xN = int((xrange[1]-xrange[0])/xstep)
+  x=np.arange(xrange[0],xrange[1],xstep).astype('float32')
+  
+  # 5 distributions containing 10 1-d points
+  distributions = np.random.rand(4,5,1,1).astype('float32')
+  base = np.random.rand(1,5,1,1).astype('float32')
+  
+  divergences = am.kl_divergence(distributions, base, base.reshape(1,1,5,1))
+  
+  for i in range(4):
+    ax = plt.subplot(2,2,i+1, title="Divergence: %s" % divergences[i])
+    
+    ax.plot(x, am.parzen_function( distributions[i].reshape(1,5,1,1), x.reshape(1,1,xN,1) ).reshape(xN), 'b' )
+    ax.plot(x, am.parzen_function( base.reshape(1,5,1,1), x.reshape(1,1,xN,1) ).reshape(xN), 'g--' )
+    ax.axis([0,1,0,None])
+    
+  print divergences
+  print divergences.shape
+  
+  plt.show()
+  
+  
+def test_parzen():
 	print "Starting"
 	
 	xrange = [0,1]